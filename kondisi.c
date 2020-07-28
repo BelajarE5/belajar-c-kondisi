894bcccec87c04732629976859a480d985b2433a
BelajarE5 <67135896+BelajarE5@users.noreply.github.com>
2020-07-29 04:56:03 +0700

    Create kondisi.c

---
+++ kondisi.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+#include"conio.h"
+
+main(){
+clrscr();
+float nilai;
+printf("Masukkan nilai yang didapat: ");
+scanf("%f ,&nilai);
+if(nilai>80)
+printf("\n ANDA LULUS!!\n");
+else
+printf("\n ANDA TIDAK LULUS!!\n");
+getch();
+}
